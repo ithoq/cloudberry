6acdffea818535e61a034d62ab2be6db4e603079
Samuli Järvelä <samuli@jarvela.com>
2014-12-09 18:58:56 +0200

    popover

--- client/app/app.js
+++ client/app/app.js
@@ -9,6 +9,7 @@ requirejs.config({
         'jquery': '../bower_components/jquery/jquery',
         'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap',
         'underscore': '../bower_components/underscore/underscore',
+        'knockout-bootstrap': '../vendor/knockout-bootstrap.min',
     },
     shim: {
         'bootstrap': {
@@ -289,19 +290,20 @@ define("cloudberry/platform", [
     "durandal/composition",
     "knockout",
     "jquery",
-    "bootstrap"
+    "bootstrap",
+    "knockout-bootstrap"
 ], function(composition, ko, $) {
-    composition.addBindingHandler("popover", {
+    /*composition.addBindingHandler("popover", {
         update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
             var options = ko.utils.unwrapObservable(valueAccessor());
-            options.content = options.template;
+            var tmpl = options.template;
             options.template = undefined;
 
-            if (options.content.startsWith('#')) {
-                var $t = $(options.content);
-                if ($t.length === 0) return;
+            if (tmpl) {
+                //var $t = $(options.content);
+                //if ($t.length === 0) return;
 
-                options.content = $t.html();
+                options.content = '<div data-bind="template: { name: \'' + tmpl + '\' }"/>';//$t.html();
                 options.html = true;
             }
 
@@ -313,8 +315,101 @@ define("cloudberry/platform", [
             else
                 $element.popover(options)
         }
-    });
+    });*/
 });
+
+/*define(['durandal/system', 'plugins/dialog', 'durandal/app', 'durandal/viewEngine', 'knockout'], function(system, dialog, app, viewEngine, ko) {
+    dialog.addContext('bootstrapModal', {
+        blockoutOpacity: .2,
+        removeDelay: 300,
+        addHost: function(theDialog) {
+            var body = $('body');
+            var host = $('<div class="modal fade" id="bootstrapModal" tabindex="-1" role="dialog" data-keyboard="false" aria-labelledby="bootstrapModal" aria-hidden="true"></div>')
+                .appendTo(body);
+            theDialog.host = host.get(0);
+        },
+        removeHost: function(theDialog) {
+            $('#bootstrapModal').modal('hide');
+            $('body').removeClass('modal-open');
+        },
+        attached: null,
+        compositionComplete: function(child, parent, context) {
+            var theDialog = dialog.getDialog(context.model);
+            var options = {};
+            options.show = true;
+            $('#bootstrapModal').modal(options);
+            $('#bootstrapModal').on('hidden.bs.modal', function(e) {
+                theDialog.close();
+                ko.removeNode(theDialog.host);
+                $('.modal-backdrop').remove();
+            });
+        }
+    });
+    var bootstrapMarkup = [
+        '<div data-view="plugins/messageBox" data-bind="css: getClass(), style: getStyle()">',
+        '<div class="modal-content">',
+        '<div class="modal-header">',
+        '<h3 data-bind="html: title"></h3>',
+        '</div>',
+        '<div class="modal-body">',
+        '<p class="message" data-bind="html: message"></p>',
+        '</div>',
+        '<div class="modal-footer">',
+        '<!-- ko foreach: options -->',
+        '<button data-bind="click: function () { $parent.selectOption($parent.getButtonValue($data)); }, text: $parent.getButtonText($data), css: $parent.getButtonClass($index)"></button>',
+        '<!-- /ko -->',
+        '<div style="clear:both;"></div>',
+        '</div>',
+        '</div>',
+        '</div>'
+    ].join('\n');
+    var bootstrapModal = function() {};
+    bootstrapModal.install = function() {
+        app.showBootstrapDialog = function(obj, activationData) {
+            return dialog.show(obj, activationData, 'bootstrapModal');
+        };
+        app.showBootstrapMessage = function(message, title, options, autoclose, settings) {
+            return dialog.showBootstrapMessage(message, title, options, autoclose, settings);
+        };
+
+        dialog.showBootstrapDialog = function(obj, activationData) {
+            return dialog.show(obj, activationData, 'bootstrapModal');
+        }
+        dialog.showBootstrapMessage = function(message, title, options, autoclose, settings) {
+            if (system.isString(this.MessageBox)) {
+                return dialog.show(this.MessageBox, [
+                    message,
+                    title || this.MessageBox.defaultTitle,
+                    options || this.MessageBox.defaultOptions,
+                    autoclose || false,
+                    settings || {}
+                ], 'bootstrapModal');
+            }
+            var bootstrapDefaults = {
+                buttonClass: "btn btn-default",
+                primaryButtonClass: "btn-primary autofocus",
+                secondaryButtonClass: "",
+                "class": "modal-dialog",
+                style: null
+            };
+            this.MessageBox.prototype.getView = function() {
+                return viewEngine.processMarkup(bootstrapMarkup);
+            };
+            var bootstrapSettings = $.extend(bootstrapDefaults, settings);
+            return dialog.show(new dialog.MessageBox(message, title, options, autoclose, bootstrapSettings), {}, 'bootstrapModal');
+        };
+        dialog.MessageBox.prototype.compositionComplete = function(child, parent, context) {
+            var theDialog = dialog.getDialog(context.model);
+            var $child = $(child);
+            if ($child.hasClass('autoclose') || context.model.autoclose) {
+                $(theDialog.blockout).click(function() {
+                    theDialog.close();
+                });
+            }
+        };
+    };
+    return bootstrapModal;
+});*/
 
 if (!window.isArray)
     window.isArray = function(o) {
--- client/app/viewmodels/login.js
+++ client/app/viewmodels/login.js
@@ -2,13 +2,17 @@ define(['cloudberry/session', 'knockout'], function(session, ko) {
     var model = {
         username : ko.observable(''),
         password : ko.observable(''),
-        remember : ko.observable(false)
+        remember : ko.observable(false),
+        resetEmail: ko.observable('')
     };
 
     return {
         model: model,
         onLogin: function() {
             session.authenticate(model.username(), model.password(), model.remember());
+        },
+        onReset: function() {
+            alert('reset');
         }
     };
 });
